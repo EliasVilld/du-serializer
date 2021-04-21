c9772172942c3737c9ce0211410b80c882d47c84
EliasVilld <82938532+EliasVilld@users.noreply.github.com>
2021-04-21 13:57:21 +0200

    Integration of the serializer lib

---
+++ serializer.lua
@@ -0,0 +1,38 @@
+-- Serialize a table to a string
+function serialize(v)
+    local t={}
+    if type(v)=="table" then
+        t[#t+1]='{'
+
+        for k,e in pairs(v)do
+            if#v==0 then t[#t+1]=k..'=' end
+            t[#t+1]=serialize(e)
+            t[#t+1]=','
+        end
+
+        if t[#t]==',' then t[#t]=nil end
+        t[#t+1]='}'
+    elseif type(v)=="number" then
+        t[#t+1]=tostring(v)
+    elseif type(v)=="string" then
+        t[#t+1]=string.format("%q",v)
+    elseif type(v)=="boolean" then
+        t[#t+1]=v and "true"or"false"
+    end
+
+	-- Remove escape characters " and '
+    t=table.concat(t):gsub(".", {['"'] = "&dq;", ["'"] = "&sq;"})
+
+    return t
+end
+
+-- Deerialize a string to a table
+function deserialize(s)
+	-- Replace escape characters " and '
+    s=s:gsub("[&dqs;]+", {["&dq;"] = '"', ["&sq;"] = "'"})
+    
+    local s="_tmp="..s
+    f=load(s)
+    if f then f() else return nil end
+    return _tmp
+end
